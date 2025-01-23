54851f40ea44356b2c7ba597bd0749b36c0ea790
AlexSim199513 <alex199513@gmail.com>
2025-01-23 13:09:57 -0500

    added draw line methods

--- main.py
+++ main.py
@@ -21,8 +21,29 @@ class Window:
     def close(self):
         self.__running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
 def main():
     win = Window(800, 600)
+    p1 = Point(0, 300)
+    p2 = Point(800, 300)
+    line1 =  Line(p1, p2)
+    win.draw_line(line1, "black")
     win.wait_for_close()
 
 # This ensures main() only runs if this file is run directly
